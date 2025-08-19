0edfc117ebeac4c83b2b2da45b1ffdd5fbc59cde
sidrxo <sidrxo@gmail.com>
2025-08-19 16:14:36 +0100

    Add venues tab for siteAdmins and venueAdmins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-// app/layout.tsx
 "use client";
+
 import "./globals.css";
 import { AppNavbar } from "@/components/app-navbar";
 import { Toaster } from "sonner";
 import RequireAuth from "@/components/require-auth";
+import { AuthProvider } from "@/lib/useAuth";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="min-h-screen bg-background text-foreground">
-        <RequireAuth>
-          <div className="min-h-screen flex flex-col">
-            <AppNavbar />
-            <main className="flex-1 p-6 max-w-[1400px] mx-auto w-full">
-              {children}
-            </main>
-          </div>
-        </RequireAuth>
+        <AuthProvider>
+          <RequireAuth>
+            <div className="min-h-screen flex flex-col">
+              <AppNavbar />
+              <main className="flex-1 p-6 max-w-[1400px] mx-auto w-full">
+                {children}
+              </main>
+            </div>
+          </RequireAuth>
+        </AuthProvider>
         <Toaster position="top-right" richColors />
       </body>
     </html>
